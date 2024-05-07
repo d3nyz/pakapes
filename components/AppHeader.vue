b7b41a8b567d246f1a473a76c5c029ad7a1157e9
d3nyz <d3n.mail@gmail.com>
2024-05-07 13:07:18 +0300

    Header improvements

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,14 +1,30 @@
 <script setup lang="ts">
 import { ref, onMounted, onBeforeUnmount } from 'vue';
 
+let lastScrollTop = 0;
 const isScrolled: Ref<boolean> = ref(false);
+const isScrolledUp: Ref<boolean> = ref(false);
+const isScrolledDown: Ref<boolean> = ref(false);
+const mainHeader: Ref<HTMLElement | null> = ref(null);
+const headerHeight: Ref<number> = ref(32);
 
 const handleScroll = () => {
   const scrollTop = window.pageYOffset || document.documentElement.scrollTop;
   isScrolled.value = scrollTop > 0;
+  if (scrollTop > lastScrollTop && scrollTop > headerHeight.value) {
+    // scrolled down
+    isScrolledDown.value = true;
+    isScrolledUp.value = false;
+  } else {
+    // scrolled up
+    isScrolledUp.value = true;
+    isScrolledDown.value = false;
+  }
+  lastScrollTop = scrollTop;
 };
 
-onMounted(() => {
+onMounted(async() => {
+  headerHeight.value = mainHeader.value?.offsetHeight || 45;
   window.addEventListener('scroll', handleScroll);
 });
 
@@ -19,22 +35,15 @@ onBeforeUnmount(() => {
 
 <template>
   <div>
-    <!-- Temporary solution to push content down by header size (to fix header over content on mobile) -->
-    <div class="header-placeholder">
-      <div class="header">
+    <div class="empty-header-placeholder" :style="{ height: `${headerHeight}px` }"></div>
+    <header ref="mainHeader" class="header" :class="{ 'header-scrolled': isScrolled, 'header-scrolled-up': isScrolledUp, 'header-scrolled-down': isScrolledDown }">
+      <div>
         <nav class="navigation">
           <NuxtLink to="/" class="navigation-link">Sākums</NuxtLink>
           <NuxtLink to="/list" class="navigation-link">Pakāpes</NuxtLink>
           <NuxtLink to="https://www.klustikaravirs.lv/" class="navigation-link" target="_blank" hreflang="lv">Kļūsti karavīrs &#8599;</NuxtLink>
         </nav>
       </div>
-    </div>
-    <header class="header" :class="{ 'header-scrolled': isScrolled }">
-      <nav class="navigation">
-        <NuxtLink to="/" class="navigation-link">Sākums</NuxtLink>
-        <NuxtLink to="/list" class="navigation-link">Pakāpes</NuxtLink>
-        <NuxtLink to="https://www.klustikaravirs.lv/" class="navigation-link" target="_blank" hreflang="lv">Kļūsti karavīrs &#8599;</NuxtLink>
-      </nav>
     </header>
   </div>
 </template>
